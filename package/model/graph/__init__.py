2c8631a530193440da1d42117097c400db50edcf
ain-soph <ain-soph@live.com>
2020-05-21 22:07:39 -0400

    new

--- package/model/graph/__init__.py
+++
@@ -1,4 +0,0 @@
-# from .gnn import GNN
-# from .gnn_baseline import GNN_Baseline
-# from .gnn_node import GNN_Node
-# from .gnn_reverse import GNN_Reverse
--- transform_onnx2keras.py
+++
@@ -1,6 +0,0 @@
-import onnx
-from onnx2keras import onnx_to_keras
-import tensorflow as tf
-onnx_model = onnx.load('model.onnx')
-k_model = onnx_to_keras(onnx_model, ['input'])
-tf.keras.models.save_model(k_model, "./model.h5")
