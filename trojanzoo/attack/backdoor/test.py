c28193e777a830d05f21669f3cfd682c6aef05c8
Zhaohan Xi <zxx5113@lrs-twang01.ist.psu.edu>
2020-06-27 17:10:51 -0400

    add latent backdoor

--- trojanzoo/attack/backdoor/test.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-a = torch.rand((4, 3, 3))
-mask = torch.tensor([[0, 0, 0],[0, 1, 1],[0, 1, 1],])
-mark = torch.tensor([[-1, -1, -1],[-1, -1, -1],[-1, -1, -1]])
-
-a = a * (1-mask) + mask * mark
-
-print(a)
