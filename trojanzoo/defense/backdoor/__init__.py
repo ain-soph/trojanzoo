050e7b88947066d59358c9dd36226501d2c39baa
corazju <corazju@zju.edu.cn>
2020-07-22 17:07:41 +0800

    spectral sinature v1

--- trojanzoo/defense/backdoor/__init__.py
+++ trojanzoo/defense/backdoor/__init__.py
@@ -6,3 +6,4 @@ from .abs import ABS
 from .fine_pruning import Fine_Pruning
 from .activation_clustering import Activation_Clustering
 from .deep_inspect import Deep_Inspect
+from .spectral_signature import Spectral_Signature
