e282320d7a5247b06cffc56a5f883e6bdc2ce038
ain-soph <ain-soph@live.com>
2020-09-24 15:56:25 -0400

    fix training activate_params bug

--- trojanzoo/attack/backdoor/imc_latent.py
+++ trojanzoo/attack/backdoor/imc_latent.py
@@ -27,20 +27,22 @@ class IMC_Latent(Latent_Backdoor):
 
     def attack(self, **kwargs):
         print('Sample Data')
-        data = self.sample_data()
-        print('Calculate Average Target Features')
-        self.avg_target_feats = self.get_avg_target_feats(data)
+        self.data = self.sample_data()
+        # print('Calculate Average Target Features')
+        # self.avg_target_feats = self.get_avg_target_feats(self.data)
         print('Retrain')
-        return super().attack(epoch_func=self.epoch_func, **kwargs)
+        return super(Latent_Backdoor, self).attack(epoch_func=self.epoch_func, **kwargs)
 
     def epoch_func(self, **kwargs):
-        if self.model.sgm and 'sgm_remove' not in self.model.__dict__.keys():
-            register_hook(self.model, self.model.sgm_gamma)
-        for data in self.dataset.loader['train']:
-            _input, _label = self.model.get_data(data)
-            adv_input, _iter = self.pgd.optimize(_input, noise=self.mark.mark, add_noise_fn=self.mark.add_mark)
-        if self.model.sgm:
-            remove_hook(self.model)
+        self.avg_target_feats = self.get_avg_target_feats(self.data)
+        self.preprocess_mark(data=self.data)
+        # if self.model.sgm and 'sgm_remove' not in self.model.__dict__.keys():
+        #     register_hook(self.model, self.model.sgm_gamma)
+        # for data in self.dataset.loader['train']:
+        #     _input, _label = self.model.get_data(data)
+        #     adv_input, _iter = self.pgd.optimize(_input, noise=self.mark.mark, add_noise_fn=self.mark.add_mark)
+        # if self.model.sgm:
+        #     remove_hook(self.model)
 
     def loss_mse(self, poison_x: torch.Tensor) -> torch.Tensor:
         other_feats = self.model.get_layer(poison_x, layer_output=self.preprocess_layer)
