e7c36110e254c765ec08fa5dbd800c40e491db19
ain-soph <ain-soph@live.com>
2020-11-02 19:36:36 -0500

    update figure1_size

--- figure1_size.py
+++ figure1_size.py
@@ -12,19 +12,16 @@ warnings.filterwarnings("ignore")
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--dataset', dest='dataset', default='cifar10')
-    parser.add_argument('-c', '--confidence', dest='confidence', action='store_true')
     args = parser.parse_args()
     name = 'figure1 %s size' % args.dataset
-    if args.confidence:
-        name += ' confidence'
     fig = Figure(name)
     fig.set_axis_label('x', 'Trigger Size')
-    fig.set_axis_label('y', 'Misclassification Confidence' if args.confidence else 'Max Re-Mask Accuracy')
-    if args.dataset == 'cifar10':
-        fig.set_axis_lim('x', lim=[0, 7], piece=7, margin=[0, 0.5],
-                         _format='%d')
+    fig.set_axis_label('y', 'Max Re-Mask Accuracy')
     if args.dataset == 'gtsrb':
         fig.set_axis_lim('x', lim=[0, 10], piece=10, margin=[0, 0.5],
+                         _format='%d')
+    else:
+        fig.set_axis_lim('x', lim=[0, 7], piece=7, margin=[0, 0.5],
                          _format='%d')
     fig.set_axis_lim('y', lim=[0, 100], piece=5, margin=[0.0, 5.0],
                      _format='%d')
@@ -70,22 +67,7 @@ if __name__ == '__main__':
             # 'bypassing': [82.600, 79.800, 77.800, 74.000, 72.400, 69.400, 46.800, 10.800, 10.600, 10.600],
         },
     }
-    z = {
-        # 'cifar10': {
-        #     'badnet': [0.652851402759552, 0.5328619480133057, 0.6471461653709412, 0.7925588488578796, 0.8418892621994019, 0.7844262719154358, 0.8106977343559265],
-        #     'latent_backdoor': [0.5402058362960815, 0.9261912703514099, 0.9638524651527405, 0.9476844668388367, 0.9828152060508728, 0.9868102073669434, 0.8296664357185364],
-        #     'trojannn': [0.4948200583457947, 0.5762385725975037, 0.7242116332054138, 0.6653879880905151, 0.580232560634613, 0.508546769618988, 0.5213910937309265],
-        #     'imc': [0.5129220485687256, 0.7297095060348511, 0.6254073977470398, 0.4126419723033905],
-        #     'clean_label_pgd': [0.8591371178627014, 0.8928311467170715, 0.9218209385871887, 0.931251049041748, 0.9373565912246704, 0.9461389780044556, 0.9672442078590393],
-        # },
-    }
-    for key in z.keys():
-        for sub_key in z[key].keys():
-            for i in range(len(z[key][sub_key])):
-                z[key][sub_key][i] *= 100
-    y = z if args.confidence else y
     for i, (key, value) in enumerate(y[args.dataset].items()):
-        print(key)
         fig.curve(x[:len(value)], value, color=color_list[i], label=key)
         fig.scatter(x[:len(value)], value, color=color_list[i], marker=mark_list[i])
     fig.save('./result/')
