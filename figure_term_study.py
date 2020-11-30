460bab5b0baa52f69d235f671eb9d24c10fe5c9f
themoonboy <themoonboy@yeah.net>
2020-11-29 20:19:37 -0500

    update plot figure-term

--- figure_term_study.py
+++ figure_term_study.py
@@ -12,13 +12,13 @@ warnings.filterwarnings("ignore")
 if __name__ == '__main__':
     name = 'figure Attack Optimization Term Study'
     fig = Figure(name)
-    fig.set_axis_label('x', 'Trigger Transparency')
-    fig.set_axis_label('y', 'Max Re-Mask Accuracy')
-    fig.set_axis_lim('x', lim=[0, 1.0], piece=10, margin=[0.05, 0.05],
+    fig.set_axis_label('x', r'Trigger Transparency ($\mathbf{\alpha}$)')
+    fig.set_axis_label('y', 'ASR (%)')
+    fig.set_axis_lim('x', lim=[0, 0.9], piece=9, margin=[0.05, 0.05],
                      _format='%.1f')
-    fig.set_axis_lim('y', lim=[40, 100], piece=5, margin=[0.0, 5.0],
+    fig.set_axis_lim('y', lim=[40, 100], piece=3, margin=[0.0, 5.0],
                      _format='%d')
-    fig.set_title(fig.name)
+    fig.set_title('')
 
     mark_dict = {
         'badnet': 'H',
@@ -58,7 +58,30 @@ if __name__ == '__main__':
         'latent_backdoor': [100.000, 100.000, 100.000, 100.000, 100.000, 99.990, 99.880, 99.410, 97.730, 95.370],
     }
     for i, (key, value) in enumerate(y.items()):
-        fig.curve(x[:len(value)], value, color=color_dict[key], marker=mark_dict[key], label=attack_mapping[key])
-        fig.scatter(x[:len(value)], value, color=color_dict[key], marker=mark_dict[key])
-    fig.set_legend()
+        # if key != 'trojannn':
+        #     continue
+
+        y_list = np.array(y[key])
+        x_list = np.array(x[:len(y_list)])
+        x_grid = np.linspace(0.0, 0.9, 9000)
+        y_grid = np.linspace(0.0, 0.9, 9000)
+
+        if key in ['imc', 'latent_backdoor']:
+            y_grid = fig.interp_fit(x_list, y_list, x_grid)
+            y_grid = np.clip(y_grid, a_min=0.0, a_max=100.0)
+            y_grid = fig.monotone(y_grid, increase=False)
+            y_grid = fig.avg_smooth(y_grid, window=100)
+            y_grid = fig.avg_smooth(y_grid, window=200)
+            y_grid = fig.avg_smooth(y_grid, window=300)
+            y_grid = fig.avg_smooth(y_grid, window=400)
+        if key in ['badnet', 'trojannn']:
+            y_grid = fig.exp_fit(x_list, y_list, x_grid, degree=5, increase=False, epsilon=5)
+            y_grid = fig.monotone(y_grid, increase=False)
+
+        fig.curve(x_grid, y_grid, color=color_dict[key])
+        fig.scatter(x_list, y_list, color=color_dict[key], marker=mark_dict[key], label=attack_mapping[key])
+
+        # fig.curve(x[:len(value)], value, color=color_dict[key], marker=mark_dict[key], label=attack_mapping[key])
+        # fig.scatter(x[:len(value)], value, color=color_dict[key], marker=mark_dict[key])
+    fig.set_legend(ncol=1, labelspacing=0.2, loc='lower left')
     fig.save(folder_path='./result/')
