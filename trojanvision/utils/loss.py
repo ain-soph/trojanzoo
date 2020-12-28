628af2d252e7be5801d63c92397a0a0b47904cda
ain-soph <ain-soph@live.com>
2020-12-28 14:25:09 -0500

    reorganize trojanvision utils

---
+++ trojanvision/utils/loss.py
@@ -0,0 +1,54 @@
+# coding: utf-8
+
+import torch
+
+
+def total_variation(images: torch.Tensor, reduction: str = 'sum') -> torch.Tensor:
+    """Calculate and return the total variation for one or more images.
+
+    The total variation is the sum of the absolute differences for neighboring
+    pixel-values in the input images. This measures how much noise is in the
+    images.
+
+    This can be used as a loss-function during optimization so as to suppress
+    noise in images. If you have a batch of images, then you should calculate
+    the scalar loss-value as the sum:
+    `loss = tf.reduce_sum(tf.image.total_variation(images))`
+
+    This implements the anisotropic 2-D version of the formula described here:
+
+    https://en.wikipedia.org/wiki/Total_variation_denoising
+
+    Args:
+        images: 4-D Tensor of shape `[batch, channels, height, width]` or 3-D Tensor
+        of shape `[channels, height, width]`.
+
+    Raises:
+        ValueError: if images.shape is not a 3-D or 4-D vector.
+
+    Returns:
+        The total variation of `images`.
+
+        If `images` was 4-D, return a 1-D float Tensor of shape `[batch]` with the
+        total variation for each image in the batch.
+        If `images` was 3-D, return a scalar float with the total variation for
+        that image.
+    """
+    if len(images.shape) == 3:
+        images = images.unsqueeze(0)
+    # Calculate the difference of neighboring pixel-values.
+    # The images are shifted one pixel along the height and width by slicing.
+    pixel_dif1 = images[:, :, 1:, :] - images[:, :, :-1, :]
+    pixel_dif2 = images[:, :, :, 1:] - images[:, :, :, :-1]
+    # Calculate the total variation by taking the absolute value of the
+    # pixel-differences and summing over the appropriate axis.
+    tot_var1 = pixel_dif1.abs().flatten(start_dim=1).sum(dim=1)
+    tot_var2 = pixel_dif2.abs().flatten(start_dim=1).sum(dim=1)
+    tot_var = tot_var1 + tot_var2
+    if reduction is None:
+        return tot_var
+    elif reduction == 'mean':
+        return tot_var.mean()
+    elif reduction == 'sum':
+        return tot_var.sum()
+    return tot_var
---
+++ trojanvision/utils/model.py
@@ -0,0 +1,122 @@
+# coding: utf-8
+
+import torch.nn.init as init
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.modules.conv import _ConvNd
+from torch.nn.modules.utils import _pair
+
+
+def weight_init(m: nn.Module):
+    # Function for Initialization
+    '''
+    Usage:
+        model = Model()
+        model.apply(weight_init)
+    '''
+    if hasattr(m, 'reset_parameters'):
+        return m.reset_parameters()
+    for layer in m.children():
+        if hasattr(layer, 'reset_parameters'):
+            return layer.reset_parameters()
+    if isinstance(m, nn.Conv1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.Conv3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose1d):
+        init.normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose2d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.ConvTranspose3d):
+        init.xavier_normal_(m.weight.data)
+        if m.bias is not None:
+            init.normal_(m.bias.data)
+    elif isinstance(m, nn.BatchNorm1d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.BatchNorm3d):
+        init.normal_(m.weight.data, mean=1, std=0.02)
+        init.constant_(m.bias.data, 0)
+    elif isinstance(m, nn.Linear):
+        init.xavier_normal_(m.weight.data)
+        init.normal_(m.bias.data)
+    elif isinstance(m, nn.LSTM):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.LSTMCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRU):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+    elif isinstance(m, nn.GRUCell):
+        for param in m.parameters():
+            if len(param.shape) >= 2:
+                init.orthogonal_(param.data)
+            else:
+                init.normal_(param.data)
+
+
+def conv2d_same_padding(input, weight, bias=None, stride=1, padding=1, dilation=1, groups=1):
+    """
+    Conv2d layer with padding=same
+    the padding param here is not important
+    """
+
+    input_rows = input.size(2)
+    filter_rows = weight.size(2)
+    effective_filter_size_rows = (filter_rows - 1) * dilation[0] + 1
+    out_rows = (input_rows + stride[0] - 1) // stride[0]
+    padding_rows = max(0, (out_rows - 1) * stride[0]
+                       + (filter_rows - 1) * dilation[0] + 1 - input_rows)
+    rows_odd = (padding_rows % 2 != 0)
+    padding_cols = max(0, (out_rows - 1) * stride[0]
+                       + (filter_rows - 1) * dilation[0] + 1 - input_rows)
+    cols_odd = (padding_rows % 2 != 0)
+
+    if rows_odd or cols_odd:
+        input = F.pad(input, [0, int(cols_odd), 0, int(rows_odd)])
+
+    return F.conv2d(input, weight, bias, stride[0],
+                    padding=(padding_rows // 2, padding_cols // 2),
+                    dilation=dilation, groups=groups)
+
+
+class Conv2d_SAME(_ConvNd):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
+                 padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'):
+        kernel_size = _pair(kernel_size)
+        stride = _pair(stride)
+        padding = _pair(padding)
+        dilation = _pair(dilation)
+        super(Conv2d_SAME, self).__init__(
+            in_channels, out_channels, kernel_size, stride, padding, dilation,
+            False, _pair(0), groups, bias, padding_mode)
+
+    def forward(self, input):
+        return conv2d_same_padding(input, self.weight, self.bias, self.stride,
+                                   self.padding, self.dilation, self.groups)
