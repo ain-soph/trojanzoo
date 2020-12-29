7ed5e4cb651dd007c4b694179d8905bc252f81af
ain-soph <ain-soph@live.com>
2020-12-29 16:26:01 -0500

    fix trainer bugs

--- trojanvision/trainer.py
+++ trojanvision/trainer.py
@@ -1,3 +1,13 @@
 # coding: utf-8
 
-from trojanzoo.trainer import *
+from trojanvision.configs import Config, config
+from trojanvision.datasets import ImageSet
+from trojanvision.models import ImageModel
+import trojanzoo.trainer
+from trojanzoo.trainer import Trainer, add_argument
+
+
+def create(dataset_name: str = None, dataset: ImageSet = None, model: ImageModel = None,
+           config: Config = config, **kwargs):
+    return trojanzoo.trainer.create(dataset_name=dataset_name, dataset=dataset,
+                                    model=model, config=config, **kwargs)
