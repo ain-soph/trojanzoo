eb9e52b0e26a0d2182cdf4a18f4e0988d4c58193
ain-soph <ain-soph@live.com>
2020-12-29 04:58:03 -0500

    modify trojanvision.models.create

--- trojanvision/models/__init__.py
+++ trojanvision/models/__init__.py
@@ -6,6 +6,7 @@ from .alexnet import AlexNet
 from .resnet import ResNet, ResNetcomp
 from .vgg import VGG, VGGcomp
 from .densenet import DenseNet, DenseNetcomp
+from .bit import BiT
 from .magnet import MagNet
 from trojanvision.datasets import ImageSet
 from trojanvision.configs import Config, config
@@ -13,6 +14,7 @@ import trojanzoo.models
 from trojanzoo.utils import get_name
 
 import argparse
+import os
 import re
 from typing import Union
 
@@ -25,6 +27,7 @@ class_dict: dict[str, type[ImageModel]] = {
     'vggcomp': VGGcomp,
     'densenet': DenseNet,
     'densenetcomp': DenseNetcomp,
+    'bit': BiT,
     'magnet': MagNet,
 }
 
@@ -42,20 +45,24 @@ def add_argument(parser: argparse.ArgumentParser, model_name: str = None, model:
                                          config=config, class_dict=class_dict)
 
 
-def create(model_name: str = None, model: Union[str, ImageModel] = None,
+def create(model_name: str = None, model: Union[str, ImageModel] = None, folder_path: str = None,
            dataset_name: str = None, dataset: Union[str, ImageSet] = None,
            config: Config = config, class_dict: dict[str, type[ImageModel]] = class_dict, **kwargs) -> ImageModel:
     dataset_name = get_name(name=dataset_name, module=dataset, arg_list=['-d', '--dataset'])
+    model_name = get_name(name=model_name, module=model, arg_list=['-m', '--model'])
     if dataset_name is None:
         dataset_name = config.get_full_config()['dataset']['default_dataset']
-    model_name = get_name(name=model_name, module=model, arg_list=['-m', '--model'])
     if model_name is None:
         model_name = config.get_config(dataset_name=dataset_name)['model']['default_model']
-    return trojanzoo.models.create(model_name=model_name, model=model,
-                                   dataset_name=dataset_name, dataset=dataset,
-                                   config=config, class_dict=class_dict, **kwargs)
+    result = config.get_config(dataset_name=dataset_name)['model']._update(kwargs)
+    model_name = model_name if model_name is not None else result['default_model']
+
+    ModelType: type[ImageModel] = class_dict[split_model_name(model_name)]
+    if folder_path is None and isinstance(dataset, ImageSet):
+        folder_path = os.path.join(result['model_dir'], dataset.data_type, dataset.name)
+    return ModelType(name=model_name, dataset=dataset, folder_path=folder_path, **result)
 
 
 def split_model_name(name: str) -> str:
-    name: str = re.findall(r'[0-9]+|[a-z]+|_', name)[0]
+    name: str = re.findall(r'[0-9]+|[A-Za-z]+|_', name)[0]
     return name.split('-')[0].lower()
