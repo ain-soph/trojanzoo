11ce7eba7917be51d98578b1bec82a6474b2747b
ain-soph <ain-soph@live.com>
2021-01-01 19:03:59 -0500

    add heatmap example draft

---
+++ examples/heatmap.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+
+import trojanvision.environ
+import trojanvision.datasets
+import trojanvision.models
+from trojanvision.utils import save_tensor_as_img
+from trojanvision.utils import summary, superimpose
+import argparse
+
+import warnings
+warnings.filterwarnings("ignore")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    trojanvision.environ.add_argument(parser)
+    trojanvision.datasets.add_argument(parser)
+    trojanvision.models.add_argument(parser)
+    args = parser.parse_args()
+
+    env = trojanvision.environ.create(**args.__dict__)
+    dataset = trojanvision.datasets.create(**args.__dict__)
+    model = trojanvision.models.create(dataset=dataset, **args.__dict__)
+
+    if env['verbose']:
+        summary(env=env, dataset=dataset, model=model)
+    for data in dataset.loader['valid']:
+        _input, _label = model.get_data(data)
+        heatmap = model.get_heatmap(_input, _label, method='saliency_map')
+        heatmap = superimpose(heatmap, _input, alpha=0.5)
+        for i, map in enumerate(heatmap):
+            save_tensor_as_img(f'./result/{i}.jpg', heatmap[i])
+        break
