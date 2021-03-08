02d3295ba09f0b671557c8d4bff653c1ccc61c64
ain-soph <ain-soph@live.com>
2021-03-07 20:01:05 -0500

    implement dla

---
+++ trojanvision/models/dla.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+from .imagemodel import _ImageModel, ImageModel
+from trojanvision.utils.dla import BasicBlock, Tree, SimpleTree
+
+import torch.nn as nn
+
+
+class _DLA(_ImageModel):
+
+    def __init__(self, **kwargs):
+        super().__init__(conv_dim=512, fc_depth=1, **kwargs)
+
+    @staticmethod
+    def define_features(block=BasicBlock, simple: bool = False) -> nn.Sequential:
+        TreeClass = SimpleTree if simple else Tree
+        features = nn.Sequential()
+        features.add_module('base', nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        ))
+        features.add_module('layer1', nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        ))
+        features.add_module('layer2', nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True)
+        ))
+        features.add_module('layer2', TreeClass(block, 32, 64, level=1, stride=1))
+        features.add_module('layer3', TreeClass(block, 32, 64, level=1, stride=1))
+        features.add_module('layer4', TreeClass(block, 32, 64, level=1, stride=1))
+        features.add_module('layer5', TreeClass(block, 32, 64, level=1, stride=1))
+        features.add_module('layer6', TreeClass(block, 32, 64, level=1, stride=1))
+        return features
+
+
+class DLA(ImageModel):
+
+    def __init__(self, name: str = 'dla', simple: bool = None,
+                 model_class: type[_DLA] = _DLA, **kwargs):
+        if name == 'dlasimple' and simple is None:
+            simple = True
+        elif simple:
+            name = 'dlasimple'
+        super().__init__(name=name, model_class=model_class, simple=simple, **kwargs)
---
+++ trojanvision/utils/dla.py
@@ -0,0 +1,130 @@
+'''DLA in PyTorch.
+
+Reference:
+    Deep Layer Aggregation. https://arxiv.org/abs/1707.06484
+'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Root(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=1):
+        super(Root, self).__init__()
+        self.conv = nn.Conv2d(
+            in_channels, out_channels, kernel_size,
+            stride=1, padding=(kernel_size - 1) // 2, bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+
+    def forward(self, xs):
+        x = torch.cat(xs, 1)
+        out = F.relu(self.bn(self.conv(x)))
+        return out
+
+
+class Tree(nn.Module):
+    def __init__(self, block, in_channels, out_channels, level=1, stride=1):
+        super(Tree, self).__init__()
+        self.level = level
+        if level == 1:
+            self.root = Root(2 * out_channels, out_channels)
+            self.left_node = block(in_channels, out_channels, stride=stride)
+            self.right_node = block(out_channels, out_channels, stride=1)
+        else:
+            self.root = Root((level + 2) * out_channels, out_channels)
+            for i in reversed(range(1, level)):
+                subtree = Tree(block, in_channels, out_channels,
+                               level=i, stride=stride)
+                self.__setattr__('level_%d' % i, subtree)
+            self.prev_root = block(in_channels, out_channels, stride=stride)
+            self.left_node = block(out_channels, out_channels, stride=1)
+            self.right_node = block(out_channels, out_channels, stride=1)
+
+    def forward(self, x):
+        xs = [self.prev_root(x)] if self.level > 1 else []
+        for i in reversed(range(1, self.level)):
+            level_i = self.__getattr__('level_%d' % i)
+            x = level_i(x)
+            xs.append(x)
+        x = self.left_node(x)
+        xs.append(x)
+        x = self.right_node(x)
+        xs.append(x)
+        out = self.root(xs)
+        return out
+
+
+class SimpleTree(nn.Module):
+    def __init__(self, block, in_channels, out_channels, level=1, stride=1):
+        super(Tree, self).__init__()
+        self.root = Root(2 * out_channels, out_channels)
+        if level == 1:
+            self.left_tree = block(in_channels, out_channels, stride=stride)
+            self.right_tree = block(out_channels, out_channels, stride=1)
+        else:
+            self.left_tree = Tree(block, in_channels,
+                                  out_channels, level=level - 1, stride=stride)
+            self.right_tree = Tree(block, out_channels,
+                                   out_channels, level=level - 1, stride=1)
+
+    def forward(self, x):
+        out1 = self.left_tree(x)
+        out2 = self.right_tree(out1)
+        out = self.root([out1, out2])
+        return out
+
+
+class DLA(nn.Module):
+    def __init__(self, block: nn.Module = BasicBlock, num_classes: int = 10, simple: bool = False):
+        super().__init__()
+        TreeClass = SimpleTree if simple else Tree
+        self.features = nn.Sequential()
+        self.features.add_module('base', nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        ))
+        self.features.add_module('layer1', nn.Sequential(
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True)
+        ))
+        self.features.add_module('layer2', nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True)
+        ))
+        self.features.add_module('layer2', TreeClass(block, 32, 64, level=1, stride=1))
+        self.features.add_module('layer3', TreeClass(block, 32, 64, level=1, stride=1))
+        self.features.add_module('layer4', TreeClass(block, 32, 64, level=1, stride=1))
+        self.features.add_module('layer5', TreeClass(block, 32, 64, level=1, stride=1))
+        self.features.add_module('layer6', TreeClass(block, 32, 64, level=1, stride=1))
+        self.linear = nn.Linear(512, num_classes)
