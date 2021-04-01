b6148a513e294e8eee37202bd092a45fac21e641
ain-soph <ain-soph@live.com>
2021-03-31 22:22:45 -0400

    modify cifar10 dataset

--- trojanvision/datasets/cifar.py
+++ trojanvision/datasets/cifar.py
@@ -5,6 +5,8 @@ from .imageset import ImageSet
 
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
+
+import argparse
 from typing import Union
 
 
@@ -16,37 +18,41 @@ class CIFAR10(ImageSet):
     class_to_idx = {'airplane': 0, 'automobile': 1, 'bird': 2, 'cat': 3,
                     'deer': 4, 'dog': 5, 'frog': 6, 'horse': 7, 'ship': 8, 'truck': 9}
 
+    @classmethod
+    def add_argument(cls, group: argparse._ArgumentGroup):
+        super().add_argument(group)
+        group.add_argument('--cutout', action='store_true', help='use cutout')
+        group.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+
     def __init__(self, norm_par: dict[str, list[float]] = {'mean': [0.4914, 0.4822, 0.4465],
                                                            'std': [0.2023, 0.1994, 0.2010], },
-                 **kwargs):
-        return super().__init__(norm_par=norm_par, **kwargs)
+                 cutout: bool = False, cutout_length: int = 16, **kwargs):
+        self.cutout = cutout
+        self.cutout_length = cutout_length
+        super().__init__(norm_par=norm_par, **kwargs)
+        if cutout:
+            self.param_list['cifar10'] = ['cutout_length']
 
     def initialize(self):
         datasets.CIFAR10(root=self.folder_path, train=True, download=True)
         datasets.CIFAR10(root=self.folder_path, train=False, download=True)
 
-    # @staticmethod
-    # def get_transform(mode: str) -> Union[transforms.Compose, transforms.ToTensor]:
-    #     if mode == 'train':
-    #         transform = transforms.Compose([
-    #             transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
-    #             transforms.ToTensor()])
-    #     else:
-    #         transform = transforms.ToTensor()
-    #     return transform
-    @staticmethod
-    def get_transform(mode: str) -> Union[transforms.Compose, transforms.ToTensor]:
-        if mode == 'train':
-            transform = transforms.Compose([
-                transforms.RandomCrop((32, 32), padding=4),
-                transforms.RandomHorizontalFlip(),
-                # transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
-                transforms.ToTensor(),
-                # transforms.RandomErasing(value=self.norm_par['mean'])
-            ])
-        else:
-            transform = transforms.ToTensor()
-        return transform
+    def get_transform(self, mode: str) -> Union[transforms.Compose, transforms.ToTensor]:
+        if mode != 'train':
+            return transforms.ToTensor()
+        transform_list = [
+            transforms.RandomCrop((32, 32), padding=4),
+            transforms.RandomHorizontalFlip(),
+            # transforms.AutoAugment(transforms.AutoAugmentPolicy.CIFAR10),
+            transforms.ToTensor(),
+        ]
+        if self.cutout:
+            # transforms.RandomErasing(value=self.norm_par['mean'])
+            from trojanvision.utils.data import Cutout
+            import torch
+            fill_values = torch.tensor(self.norm_par['mean']).view(-1, 1, 1)
+            transform_list.append(Cutout(self.cutout_length, fill_values=fill_values))
+        return transforms.Compose(transform_list)
 
     def get_org_dataset(self, mode: str, transform: Union[str, object] = 'default', **kwargs) -> datasets.CIFAR10:
         assert mode in ['train', 'valid']
