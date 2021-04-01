08776d09d78f60f719fbd289aca21065e0625d10
ain-soph <ain-soph@live.com>
2021-03-31 22:22:19 -0400

    fix Cutout

--- trojanvision/utils/data.py
+++ trojanvision/utils/data.py
@@ -137,17 +137,18 @@ class ZipFolder(DatasetFolder):
 
 
 class Cutout:
-    def __init__(self, length: int):
+    def __init__(self, length: int, fill_values: Union[float, torch.Tensor] = 0.0):
         self.length = length
+        self.fill_values = fill_values
 
     def __call__(self, img: torch.Tensor):
         h, w = img.size(1), img.size(2)
-        mask = torch.ones(h, w)
-        y = random.randint(h)
-        x = random.randint(w)
+        mask = torch.ones(h, w, dtype=torch.bool, device=img.device)
+        y = random.randint(0, h)
+        x = random.randint(0, w)
         y1 = max(y - self.length // 2, 0)
         y2 = min(y + self.length // 2, h)
         x1 = max(x - self.length // 2, 0)
         x2 = min(x + self.length // 2, w)
-        mask[y1: y2, x1: x2] = 0.
-        return (mask * img).detach()
+        mask[y1: y2, x1: x2] = False
+        return (mask * img + ~mask * self.fill_values).detach()
