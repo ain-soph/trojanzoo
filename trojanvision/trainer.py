bee17bae38a55a6c4ba09724c52490427f073b0b
ain-soph <ain-soph@live.com>
2021-03-31 23:43:32 -0400

    fix trainer

--- trojanvision/trainer.py
+++ trojanvision/trainer.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 
-from trojanzoo.trainer import *
+from trojanvision.configs import Config, config
+from trojanzoo.trainer import Trainer
+import trojanzoo.trainer
+
+from typing import TYPE_CHECKING
+from trojanvision.datasets import ImageSet    # TODO: python 3.10
+from trojanvision.models import ImageModel
+import argparse
+if TYPE_CHECKING:
+    pass
+
+
+def add_argument(parser: argparse.ArgumentParser, ClassType: type[Trainer] = Trainer) -> argparse._ArgumentGroup:
+    return trojanzoo.trainer.add_argument(parser=parser, ClassType=ClassType)
+
+
+def create(dataset_name: str = None, dataset: ImageSet = None, model: ImageModel = None,
+           config: Config = config, ClassType=Trainer, **kwargs):
+    return trojanzoo.trainer.create(dataset_name=dataset_name, dataset=dataset,
+                                    ClassType=ClassType,
+                                    model=model, config=config, **kwargs)
