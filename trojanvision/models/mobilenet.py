63db8179f397b3f0fa63ee0ed03d5d81f506752f
ain-soph <ain-soph@live.com>
2021-04-03 04:00:28 -0400

    add comp option for mobilenet_v3

--- trojanvision/models/mobilenet.py
+++ trojanvision/models/mobilenet.py
@@ -33,16 +33,16 @@ class _MobileNet(_ImageModel):
             inverted_residual_setting = cifar10_inverted_residual_setting if 'comp' in sub_type else None
             _model = torchvision.models.mobilenet.mobilenet_v2(num_classes=self.num_classes,
                                                                inverted_residual_setting=inverted_residual_setting)
-            if 'comp' in sub_type:
-                conv1: nn.Conv2d = _model.features[0][0]
-                _model.features[0][0] = nn.Conv2d(conv1.in_channels, conv1.out_channels, kernel_size=conv1.kernel_size,
-                                                  stride=1, padding=conv1.padding, dilation=conv1.dilation,
-                                                  groups=conv1.groups, bias=False)
-        elif sub_type == 'v3_large':
+        elif 'v3_large' in sub_type:
             _model = torchvision.models.mobilenet.mobilenet_v3_large(num_classes=self.num_classes)
         else:
-            assert sub_type == 'v3_small'
+            assert 'v3_small' in sub_type
             _model = torchvision.models.mobilenet.mobilenet_v3_small(num_classes=self.num_classes)
+        if 'comp' in sub_type:
+            conv1: nn.Conv2d = _model.features[0][0]
+            _model.features[0][0] = nn.Conv2d(conv1.in_channels, conv1.out_channels, kernel_size=conv1.kernel_size,
+                                              stride=1, padding=conv1.padding, dilation=conv1.dilation,
+                                              groups=conv1.groups, bias=False)
         self.features = _model.features
         self.classifier = _model.classifier
 
@@ -52,10 +52,11 @@ class MobileNet(ImageModel):
     def __init__(self, name: str = 'mobilenet', model: type[_MobileNet] = _MobileNet, **kwargs):
         try:
             sub_type: str = name[10:]
-            assert sub_type in ['v2', 'v2_comp', 'v3_small', 'v3_large'], f'{name=}'
+            assert sub_type in ['v2', 'v2_comp', 'v3_small', 'v3_large', 'v3_small_comp', 'v3_large_comp'], f'{name=}'
         except:
             raise AssertionError("model name should be in ['mobilenet_v2', 'mobilenet_v2_comp', "
-                                 "'mobilenet_v3_small', 'mobilenet_v3_large']")
+                                 "'mobilenet_v3_small', 'mobilenet_v3_large', "
+                                 "'mobilenet_v3_small_comp', 'mobilenet_v3_large_comp']")
         super().__init__(name=name, model=model, sub_type=sub_type, **kwargs)
 
     def get_official_weights(self, **kwargs) -> OrderedDict[str, torch.Tensor]:
