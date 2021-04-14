691263ed9a03491f439bcd30c2eecde26a5752de
ain-soph <ain-soph@live.com>
2021-04-13 21:29:03 -0400

    optimize model utils

--- trojanvision/utils/model.py
+++ trojanvision/utils/model.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
+import torch
 import torch.nn.init as init
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.conv import _ConvNd
 from torch.nn.modules.utils import _pair
 
+from torch.types import _int, _size
+from typing import Optional, Union
 
-def weight_init(m: nn.Module):
+
+def weight_init(m: nn.Module) -> None:
     # Function for Initialization
     '''
     Usage:
@@ -19,61 +23,22 @@ def weight_init(m: nn.Module):
     for layer in m.children():
         if hasattr(layer, 'reset_parameters'):
             return layer.reset_parameters()
-    if isinstance(m, nn.Conv1d):
+    if isinstance(m, (nn.Conv1d, nn.ConvTranspose1d)):
         init.normal_(m.weight.data)
         if m.bias is not None:
             init.normal_(m.bias.data)
-    elif isinstance(m, nn.Conv2d):
+    elif isinstance(m, (nn.Conv2d, nn.Conv3d,
+                        nn.ConvTranspose2d, nn.ConvTranspose3d)):
         init.xavier_normal_(m.weight.data)
         if m.bias is not None:
             init.normal_(m.bias.data)
-    elif isinstance(m, nn.Conv3d):
-        init.xavier_normal_(m.weight.data)
-        if m.bias is not None:
-            init.normal_(m.bias.data)
-    elif isinstance(m, nn.ConvTranspose1d):
-        init.normal_(m.weight.data)
-        if m.bias is not None:
-            init.normal_(m.bias.data)
-    elif isinstance(m, nn.ConvTranspose2d):
-        init.xavier_normal_(m.weight.data)
-        if m.bias is not None:
-            init.normal_(m.bias.data)
-    elif isinstance(m, nn.ConvTranspose3d):
-        init.xavier_normal_(m.weight.data)
-        if m.bias is not None:
-            init.normal_(m.bias.data)
-    elif isinstance(m, nn.BatchNorm1d):
-        init.normal_(m.weight.data, mean=1, std=0.02)
-        init.constant_(m.bias.data, 0)
-    elif isinstance(m, nn.BatchNorm2d):
-        init.normal_(m.weight.data, mean=1, std=0.02)
-        init.constant_(m.bias.data, 0)
-    elif isinstance(m, nn.BatchNorm3d):
+    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)):
         init.normal_(m.weight.data, mean=1, std=0.02)
         init.constant_(m.bias.data, 0)
     elif isinstance(m, nn.Linear):
         init.xavier_normal_(m.weight.data)
         init.normal_(m.bias.data)
-    elif isinstance(m, nn.LSTM):
-        for param in m.parameters():
-            if param.dim() >= 2:
-                init.orthogonal_(param.data)
-            else:
-                init.normal_(param.data)
-    elif isinstance(m, nn.LSTMCell):
-        for param in m.parameters():
-            if param.dim() >= 2:
-                init.orthogonal_(param.data)
-            else:
-                init.normal_(param.data)
-    elif isinstance(m, nn.GRU):
-        for param in m.parameters():
-            if param.dim() >= 2:
-                init.orthogonal_(param.data)
-            else:
-                init.normal_(param.data)
-    elif isinstance(m, nn.GRUCell):
+    elif isinstance(m, (nn.LSTM, nn.LSTMCell, nn.GRU, nn.GRUCell)):
         for param in m.parameters():
             if param.dim() >= 2:
                 init.orthogonal_(param.data)
@@ -81,21 +46,21 @@ def weight_init(m: nn.Module):
                 init.normal_(param.data)
 
 
-def conv2d_same_padding(input, weight, bias=None, stride=1, padding=1, dilation=1, groups=1):
-    """
-    Conv2d layer with padding=same
-    the padding param here is not important
-    """
-
+def conv2d_same_padding(input: torch.Tensor, weight: torch.Tensor,
+                        bias: Optional[torch.Tensor] = None,
+                        stride: Union[_int, _size] = 1,
+                        padding: Union[_int, _size] = 1,    # useless
+                        dilation: Union[_int, _size] = 1,
+                        groups: _int = 1):
     input_rows = input.size(2)
     filter_rows = weight.size(2)
-    effective_filter_size_rows = (filter_rows - 1) * dilation[0] + 1
+    # effective_filter_size_rows = (filter_rows - 1) * dilation[0] + 1
     out_rows = (input_rows + stride[0] - 1) // stride[0]
-    padding_rows = max(0, (out_rows - 1) * stride[0]
-                       + (filter_rows - 1) * dilation[0] + 1 - input_rows)
+    padding_rows = max(0, (out_rows - 1) * stride[0] +
+                       (filter_rows - 1) * dilation[0] + 1 - input_rows)
     rows_odd = (padding_rows % 2 != 0)
-    padding_cols = max(0, (out_rows - 1) * stride[0]
-                       + (filter_rows - 1) * dilation[0] + 1 - input_rows)
+    padding_cols = max(0, (out_rows - 1) * stride[0] +
+                       (filter_rows - 1) * dilation[0] + 1 - input_rows)
     cols_odd = (padding_rows % 2 != 0)
 
     if rows_odd or cols_odd:
@@ -107,15 +72,18 @@ def conv2d_same_padding(input, weight, bias=None, stride=1, padding=1, dilation=
 
 
 class Conv2d_SAME(_ConvNd):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'):
+    def __init__(self, in_channels: int, out_channels: int,
+                 kernel_size: tuple[int, ...], stride: tuple[int, ...] = 1,
+                 padding: tuple[int, ...] = 0, dilation: tuple[int, ...] = 1,
+                 groups: int = 1, bias: bool = True, padding_mode: str = 'zeros'):
         kernel_size = _pair(kernel_size)
         stride = _pair(stride)
         padding = _pair(padding)
         dilation = _pair(dilation)
-        super(Conv2d_SAME, self).__init__(
+        super().__init__(
             in_channels, out_channels, kernel_size, stride, padding, dilation,
-            False, _pair(0), groups, bias, padding_mode)
+            transposed=False, output_padding=_pair(0),
+            groups=groups, bias=bias, padding_mode=padding_mode)
 
     def forward(self, input):
         return conv2d_same_padding(input, self.weight, self.bias, self.stride,
