a43c27da6fdeaa7c42dd159e3a18785b79cc08ba
ain-soph <ain-soph@live.com>
2021-04-13 20:47:31 -0400

    modify command comments

--- examples/unlearn_attack.py
+++ examples/unlearn_attack.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# python backdoor_unlearn.py --attack badnet --defense neural_cleanse --percent 0.01 --validate_interval 1 --epoch 50 --lr 1e-2 --mark_height 3 --mark_width 3 --mark_alpha 0.0
+# CUDA_VISIBLE_DEVICES=0 python ./examples/backdoor_unlearn.py --color --verbose 1 --attack badnet --defense neural_cleanse --percent 0.01 --validate_interval 1 --epoch 50 --lr 1e-2
 
 import trojanvision
 
