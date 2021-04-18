d2138e4fc166f66eab80ba81818734580b96dc98
ain-soph <ain-soph@live.com>
2021-04-18 02:53:49 -0400

    implement dla

--- trojanvision/models/normal/dla.py
+++ trojanvision/models/normal/dla.py
@@ -1,38 +1,81 @@
 #!/usr/bin/env python3
 from trojanvision.models.imagemodel import _ImageModel, ImageModel
-from trojanvision.utils.model_archs.dla import DLA as ModelClass
+from trojanvision.utils.model_archs import dla
 
 import torch.nn as nn
+
+import torch
 from collections import OrderedDict
+from collections.abc import Callable
+
+urls = {
+    'dla34': 'http://dl.yf.io/dla/models/imagenet/dla34-ba72cf86.pth',
+    'dla46_c': 'http://dl.yf.io/dla/models/imagenet/dla46_c-2bfd52c3.pth',
+    'dla46x_c': 'http://dl.yf.io/dla/models/imagenet/dla46x_c-d761bae7.pth',
+    'dla60x_c': 'http://dl.yf.io/dla/models/imagenet/dla60x_c-b870c45c.pth',
+    'dla60': 'http://dl.yf.io/dla/models/imagenet/dla60-24839fc4.pth',
+    'dla60x': 'http://dl.yf.io/dla/models/imagenet/dla60x-d15cacda.pth',
+    'dla102': 'http://dl.yf.io/dla/models/imagenet/dla102-d94d9790.pth',
+    'dla102x': 'http://dl.yf.io/dla/models/imagenet/dla102x-ad62be81.pth',
+    'dla102x2': 'http://dl.yf.io/dla/models/imagenet/dla102x2-262837b6.pth',
+    'dla169': 'http://dl.yf.io/dla/models/imagenet/dla169-0914e092.pth',
+    # 'dla60_res2net':
+    #     'https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-res2net/res2net_dla60_4s-d88db7f9.pth',
+    # 'dla60_res2next':
+    #     'https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-res2net/res2next_dla60_4s-d327927b.pth',
+}
 
 
 class _DLA(_ImageModel):
 
     def __init__(self, name: str = 'dla', **kwargs):
         super().__init__(**kwargs)
-        simple = True if 'simple' in name else False
-        _model = ModelClass(num_classes=self.num_classes, simple=simple)
+        ModelClass: Callable[..., dla.DLA] = getattr(dla, name.replace('_comp', ''))
+        _model = ModelClass(num_classes=self.num_classes)
         if 'comp' in name:
             conv1: nn.Conv2d = _model.features[0][0]
             _model.features[0][0] = nn.Conv2d(conv1.in_channels, conv1.out_channels,
                                               kernel_size=3, stride=1, padding=1, bias=False)
-            self.features = nn.Sequential(OrderedDict([
-                ('base', _model.features.base),
-                ('layer1', _model.features.layer1),
-                ('layer2', _model.features.layer2),
-                ('layer3', _model.features.layer3),
-                ('layer4', _model.features.layer4),
-                ('layer5', _model.features.layer5),
-                ('layer6', _model.features.layer6),
-            ]))
-        else:
-            self.features = _model.features
-        self.classifier = nn.Sequential(OrderedDict([
-            ('fc', _model.linear)
-        ]))
+        self.features = _model.features
+        self.classifier = _model.classifier
 
 
 class DLA(ImageModel):
+    available_models = ['dla', 'dla_comp',
+                        'dla34', 'dla46_c', 'dla46x_c', 'dla60x_c', 'dla60', 'dla60x',
+                        'dla102', 'dla102x', 'dla102x2', 'dla169',
+                        'dla34_comp', 'dla46_c_comp', 'dla46x_c_comp', 'dla60x_c_comp', 'dla60_comp', 'dla60x_comp',
+                        'dla102_comp', 'dla102x_comp', 'dla102x2_comp', 'dla169_comp']
+    model_urls = urls
 
-    def __init__(self, name: str = 'dla', model: type[_DLA] = _DLA, **kwargs):
-        super().__init__(name=name, model=model, **kwargs)
+    def __init__(self, name: str = 'dla', layer: int = 34, model: type[_DLA] = _DLA, **kwargs):
+        super().__init__(name=name, layer=layer, model=model, **kwargs)
+
+    def get_official_weights(self, **kwargs) -> OrderedDict[str, torch.Tensor]:
+        old_dict = super().get_official_weights(**kwargs)
+        new_dict: OrderedDict[str, torch.Tensor] = self.state_dict()
+        old_keys = list(old_dict.keys())
+        new_keys = list(new_dict.keys())
+        new2old: dict[str, str] = {}
+        i = 0
+        j = 0
+        while(i < len(new_keys) and j < len(old_keys)):
+            if 'num_batches_tracked' in new_keys[i]:
+                i += 1
+                continue
+            if 'project' not in new_keys[i] and 'project' in old_keys[j]:
+                j += 1
+                continue
+            # print(f'{new_keys[i]:60} {old_keys[j]}')
+            new2old[new_keys[i]] = old_keys[j]
+            i += 1
+            j += 1
+        for i, key in enumerate(new_keys):
+            if 'num_batches_tracked' in key:
+                new_dict[key] = torch.tensor(0)
+            elif 'fc.weight' in key:
+                weight = old_dict[new2old[key]]
+                new_dict[key] = weight.flatten(1)
+            else:
+                new_dict[key] = old_dict[new2old[key]]
+        return new_dict
--- trojanvision/utils/model_archs/dla.py
+++ trojanvision/utils/model_archs/dla.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# https://github.com/rwightman/pytorch-image-models/tree/master/timm/models
 # https://github.com/kuangliu/pytorch-cifar/blob/master/models/dla.py
 
 '''DLA in PyTorch.
@@ -7,97 +8,259 @@
 Reference:
     Deep Layer Aggregation. https://arxiv.org/abs/1707.06484
 '''
+
+import torch
 import torch.nn as nn
-from torchvision.models.resnet import BasicBlock
 from collections import OrderedDict
+from torchvision.models.resnet import conv1x1, conv3x3
+
+from collections.abc import Callable
+from typing import Optional
 
 
-def Root(in_channels: int, out_channels: int, kernel_size: int = 1) -> nn.Sequential:
-    return nn.Sequential(OrderedDict([
-        ('conv', nn.Conv2d(in_channels, out_channels, kernel_size,
-                           stride=1, padding=(kernel_size - 1) // 2,
-                           bias=False)),
-        ('bn', nn.BatchNorm2d(out_channels)),
-        ('relu', nn.ReLU(inplace=True))
-    ]))
+class BasicBlock(nn.Module):
+    expansion: int = 1
+
+    def __init__(self, inplanes: int, planes: int, stride: int = 1, dilation: int = 1) -> None:
+        super().__init__()
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        self.conv1 = conv3x3(inplanes, planes, stride=stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.stride = stride
+
+    def forward(self, x: torch.Tensor, residual: torch.Tensor = None) -> torch.Tensor:
+        if residual is None:
+            residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion: int = 2
+
+    def __init__(self, inplanes: int, planes: int, stride: int = 1,
+                 groups: int = 1, base_width: int = 64, dilation: int = 1,
+                 norm_layer: Optional[Callable[..., nn.Module]] = None) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * groups * (base_width / 64.) / self.expansion)
+
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride, groups, dilation)
+        self.bn2 = norm_layer(width)
+        self.conv3 = conv1x1(width, planes)
+        self.bn3 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.stride = stride
+
+    def forward(self, x: torch.Tensor, residual: torch.Tensor = None) -> torch.Tensor:
+        if residual is None:
+            residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Root(nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int = 1, residual: bool = False) -> None:
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size,
+                              stride=1, padding=(kernel_size - 1) // 2,
+                              bias=False)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.residual = residual
+
+    def forward(self, *xs: list[torch.Tensor]) -> torch.Tensor:
+        identity = xs[0]
+
+        out = torch.cat(xs, dim=1)
+        out = self.conv(out)
+        out = self.bn(out)
+        out = self.relu(out)
+
+        if self.residual:
+            out += identity
+
+        return out
 
 
 class Tree(nn.Module):
-    def __init__(self, block, in_channels, out_channels, level=1, stride=1):
+    def __init__(self, block: type[nn.Module], in_channels: int, out_channels: int,
+                 levels: int, stride: int = 1,
+                 level_root=False, root_dim: int = 0, root_kernel_size: int = 1,
+                 dilation: int = 1, root_residual: bool = False, **kwargs):
         super().__init__()
-        self.level = level
-        if level == 1:
-            self.root = Root(2 * out_channels, out_channels)
-            self.left_node = block(in_channels, out_channels, stride=stride)
-            self.right_node = block(out_channels, out_channels, stride=1)
+        if root_dim == 0:
+            root_dim = 2 * out_channels
+        if level_root:
+            root_dim += in_channels
+        self.downsample: nn.Module = None
+        self.project: nn.Module = None
+        if stride > 1:
+            self.downsample = nn.MaxPool2d(stride, stride=stride)
+
+        if levels == 1:
+            self.tree1 = block(in_channels, out_channels, stride,
+                               dilation=dilation, **kwargs)
+            self.tree2 = block(out_channels, out_channels, 1,
+                               dilation=dilation, **kwargs)
+            self.root = Root(root_dim, out_channels, root_kernel_size,
+                             root_residual)
+            if in_channels != out_channels:
+                # NOTE the official impl/weights have project layers in levels > 1 case that are never
+                # used, I've moved the project layer here to avoid wasted params but old checkpoints will
+                # need strict=False while loading.
+                self.project = nn.Sequential(
+                    nn.Conv2d(in_channels, out_channels,
+                              kernel_size=1, stride=1, bias=False),
+                    nn.BatchNorm2d(out_channels)
+                )
         else:
-            self.root = Root((level + 2) * out_channels, out_channels)
-            for i in reversed(range(1, level)):
-                subtree = Tree(block, in_channels, out_channels,
-                               level=i, stride=stride)
-                self.__setattr__('level_%d' % i, subtree)
-            self.prev_root = block(in_channels, out_channels, stride=stride)
-            self.left_node = block(out_channels, out_channels, stride=1)
-            self.right_node = block(out_channels, out_channels, stride=1)
+            self.tree1 = Tree(block, in_channels, out_channels, levels - 1,
+                              stride, root_dim=0,
+                              root_kernel_size=root_kernel_size,
+                              dilation=dilation, root_residual=root_residual, **kwargs)
+            self.tree2 = Tree(block, out_channels, out_channels, levels - 1,
+                              root_dim=root_dim + out_channels,
+                              root_kernel_size=root_kernel_size,
+                              dilation=dilation, root_residual=root_residual, **kwargs)
+        self.level_root = level_root
+        self.root_dim = root_dim
+        self.levels = levels
 
-    def forward(self, x):
-        xs = [self.prev_root(x)] if self.level > 1 else []
-        for i in reversed(range(1, self.level)):
-            level_i = self.__getattr__('level_%d' % i)
-            x = level_i(x)
-            xs.append(x)
-        x = self.left_node(x)
-        xs.append(x)
-        x = self.right_node(x)
-        xs.append(x)
-        out = self.root(xs)
-        return out
-
-
-class SimpleTree(nn.Module):
-    def __init__(self, block, in_channels, out_channels, level=1, stride=1):
-        super().__init__()
-        self.root = Root(2 * out_channels, out_channels)
-        if level == 1:
-            self.left_tree = block(in_channels, out_channels, stride=stride)
-            self.right_tree = block(out_channels, out_channels, stride=1)
+    def forward(self, x: torch.Tensor, residual: torch.Tensor = None,
+                children: list[torch.Tensor] = None) -> torch.Tensor:
+        children = [] if children is None else children
+        bottom = self.downsample(x) if self.downsample else x
+        residual = self.project(bottom) if self.project else bottom
+        if self.level_root:
+            children.append(bottom)
+        x1 = self.tree1(x, residual)
+        if self.levels == 1:
+            x2 = self.tree2(x1)
+            x = self.root(x2, x1, *children)
         else:
-            self.left_tree = Tree(block, in_channels,
-                                  out_channels, level=level - 1, stride=stride)
-            self.right_tree = Tree(block, out_channels,
-                                   out_channels, level=level - 1, stride=1)
-
-    def forward(self, x):
-        out1 = self.left_tree(x)
-        out2 = self.right_tree(out1)
-        out = self.root([out1, out2])
-        return out
+            children.append(x1)
+            x = self.tree2(x1, children=children)
+        return x
 
 
 class DLA(nn.Module):
-    def __init__(self, block: nn.Module = BasicBlock, num_classes: int = 10, simple: bool = False):
+    def __init__(self, block: nn.Module, levels: list[int], channels: list[int], num_classes: int = 1000,
+                 root_residual: bool = False, **kwargs):
         super().__init__()
-        TreeClass = SimpleTree if simple else Tree
         self.features = nn.Sequential(OrderedDict([
-            ('base', nn.Sequential(
-                nn.Conv2d(3, 16, kernel_size=7, stride=1, padding=3, bias=False),
-                nn.BatchNorm2d(16),
-                nn.ReLU(True)
-            )),
-            ('maxpool', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
-            ('layer1', nn.Sequential(
-                nn.Conv2d(16, 16, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(16),
-                nn.ReLU(True)
-            )),
-            ('layer2', nn.Sequential(
-                nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1, bias=False),
-                nn.BatchNorm2d(32),
-                nn.ReLU(True)
-            )),
-            ('layer3', TreeClass(block, 32, 64, level=1, stride=1)),
-            ('layer4', TreeClass(block, 64, 128, level=2, stride=2)),
-            ('layer5', TreeClass(block, 128, 256, level=2, stride=2)),
-            ('layer6', TreeClass(block, 256, 512, level=1, stride=2)),
+            ('stem', nn.Sequential(OrderedDict([
+                ('conv1', nn.Conv2d(3, channels[0], kernel_size=7, stride=1, padding=3, bias=False)),    # stride=1
+                ('bn1', nn.BatchNorm2d(16)),
+                ('relu', nn.ReLU(True)),
+                # ('maxpool', nn.MaxPool2d(kernel_size=3, stride=2, padding=1)),
+            ]))),
+            ('layer1', nn.Sequential(OrderedDict([
+                ('conv1', conv3x3(channels[0], channels[0])),
+                ('bn1', nn.BatchNorm2d(16)),
+                ('relu', nn.ReLU(True))
+            ]))),
+            ('layer2', nn.Sequential(OrderedDict([
+                ('conv1', conv3x3(channels[0], channels[1], stride=2)),
+                ('bn1', nn.BatchNorm2d(32)),
+                ('relu', nn.ReLU(True))
+            ]))),
+            ('layer3', Tree(block, channels[1], channels[2], levels=levels[0], stride=2,
+                            level_root=False, root_residual=root_residual, **kwargs)),
+            ('layer4', Tree(block, channels[2], channels[3], levels=levels[1], stride=2,
+                            level_root=True, root_residual=root_residual, **kwargs)),
+            ('layer5', Tree(block, channels[3], channels[4], levels=levels[2], stride=2,
+                            level_root=True, root_residual=root_residual, **kwargs)),
+            ('layer6', Tree(block, channels[4], channels[5], levels=levels[3], stride=2,
+                            level_root=True, root_residual=root_residual, **kwargs)),
         ]))
-        self.linear = nn.Linear(512, num_classes)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.flatten = nn.Flatten()
+        self.classifier = nn.Sequential(OrderedDict([
+            ('fc', nn.Linear(channels[-1], num_classes))
+        ]))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.features(x)
+        out = self.pool(out)
+        out = self.flatten(out)
+        out = self.classifier(out)
+        return out
+
+
+def dla34(**kwargs) -> DLA:
+    return DLA(BasicBlock, levels=[1, 2, 2, 1], channels=[16, 32, 64, 128, 256, 512], **kwargs)
+
+
+def dla46_c(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 2, 2, 1], channels=[16, 32, 64, 64, 128, 256], **kwargs)
+
+
+def dla46x_c(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 2, 2, 1], channels=[16, 32, 64, 64, 128, 256],
+               groups=32, base_width=4, **kwargs)
+
+
+def dla60x_c(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 2, 3, 1], channels=[16, 32, 64, 64, 128, 256],
+               groups=32, base_width=4, **kwargs)
+
+
+def dla60(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 2, 3, 1], channels=[16, 32, 128, 256, 512, 1024], **kwargs)
+
+
+def dla60x(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 2, 3, 1], channels=[16, 32, 128, 256, 512, 1024],
+               groups=32, base_width=4, **kwargs)
+
+
+def dla102(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 3, 4, 1], channels=[16, 32, 128, 256, 512, 1024],
+               root_residual=True, **kwargs)
+
+
+def dla102x(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 3, 4, 1], channels=[16, 32, 128, 256, 512, 1024],
+               groups=32, base_width=4, root_residual=True, **kwargs)
+
+
+def dla102x2(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[1, 3, 4, 1], channels=[16, 32, 128, 256, 512, 1024],
+               groups=64, base_width=4, root_residual=True, **kwargs)
+
+
+def dla169(**kwargs) -> DLA:
+    return DLA(Bottleneck, levels=[2, 3, 5, 1], channels=[16, 32, 128, 256, 512, 1024],
+               root_residual=True, **kwargs)
