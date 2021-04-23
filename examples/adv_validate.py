df06b7ee259f46e1c5a509fe55156b4a4c207129
ain-soph <ain-soph@live.com>
2021-04-23 13:58:37 -0400

    add a validation script

---
+++ examples/adv_validate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import trojanvision
+from trojanvision.attacks import PoisonRandom
+
+from trojanvision.utils import summary
+import argparse
+
+import warnings
+warnings.filterwarnings("ignore")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    trojanvision.environ.add_argument(parser)
+    trojanvision.datasets.add_argument(parser)
+    trojanvision.models.add_argument(parser)
+    trojanvision.trainer.add_argument(parser)
+    trojanvision.attacks.add_argument(parser)
+    args = parser.parse_args()
+
+    env = trojanvision.environ.create(**args.__dict__)
+    dataset = trojanvision.datasets.create(**args.__dict__)
+    model = trojanvision.models.create(dataset=dataset, **args.__dict__)
+    trainer = trojanvision.trainer.create(dataset=dataset, model=model, **args.__dict__)
+    attack: PoisonRandom = trojanvision.attacks.create(dataset=dataset, model=model, **args.__dict__)
+
+    if env['verbose']:
+        summary(env=env, dataset=dataset, model=model, train=trainer, attack=attack)
+    attack.load()
+    attack.validate_fn()
