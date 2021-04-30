d989b684e40b899be14dd896a93e8493fb3caf13
ain-soph <ain-soph@live.com>
2021-04-30 19:53:59 -0400

    update vggface2

--- trojanvision/datasets/folder/vggface2.py
+++ trojanvision/datasets/folder/vggface2.py
@@ -16,8 +16,10 @@ class VGGface2(ImageFolder):
     num_classes = 8631
     valid_set: bool = False
     # https://docs.qingcloud.com/product/ai/deeplearning/
-    url = {'train': 'https://appcenter-deeplearning.sh1a.qingstor.com/dataset/vggface2/vggface2_train.tar.gz'}
-    md5 = {'train': '88813c6b15de58afc8fa75ea83361d7f'}
+    url = {'train': 'https://appcenter-deeplearning.sh1a.qingstor.com/dataset/vggface2/vggface2_train.tar.gz',
+           'test': 'https://appcenter-deeplearning.sh1a.qingstor.com/dataset/vggface2/vggface2_test.tar.gz'}
+    md5 = {'train': '88813c6b15de58afc8fa75ea83361d7f',
+           'test': 'bb7a323824d1004e14e00c23974facd3'}
     org_folder_name = {'train': 'train'}
 
     # Out-Of-Date
