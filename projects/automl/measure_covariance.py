a48ac5c8494292cfeec005957ae0089e04f2a873
ain-soph <ain-soph@live.com>
2021-05-09 21:46:09 -0400

    update grad_var scripts

--- projects/automl/measure_covariance.py
+++ projects/automl/measure_covariance.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-import trojanvision
-from trojanvision.utils import summary
-
-import torch
-import numpy as np
+import trojanvision.environ
+import trojanvision.datasets
+import trojanvision.models
+from trojanvision.utils import to_numpy
 import argparse
+import torch, time
+import numpy as np
 
-seed = 40
+import warnings
+warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -20,23 +22,13 @@ if __name__ == '__main__':
     dataset = trojanvision.datasets.create(**args.__dict__)
     model = trojanvision.models.create(dataset=dataset, **args.__dict__)
 
-    if env['verbose']:
-        summary(env=env, dataset=dataset, model=model)
-    # loss, acc1 = model._validate()
-
-    import random
-    random.seed(seed)
-    np.random.seed(seed)
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-
-    # model.train()
     model.activate_params(model.parameters())
     model.zero_grad()
 
+    torch.random.manual_seed(int(time.time()))
     grad_list = []
     for i, data in enumerate(dataset.get_dataloader('valid', shuffle=True, batch_size=1, drop_last=True)):
-        if i >= 1000:
+        if i >= 1024:
             break
         _input, _label = model.get_data(data)
         loss = model.loss(_input, _label)
@@ -45,13 +37,15 @@ if __name__ == '__main__':
         for param in model.parameters():
             grad_temp_list.append(param.grad.flatten())
         grad = torch.cat(grad_temp_list)
-        norm = grad.norm(p=2)
-        if norm >= 5.0:
-            grad = grad * 5.0 / norm
+        grad = grad if grad.norm(p=2) <= 5.0 else grad / grad.norm(p=2) * 5.0
         grad_list.append(grad.detach().cpu().clone())
         model.zero_grad()
+    
     model.eval()
     model.activate_params([])
+
     grad_tensor = torch.stack(grad_list)
-    std = float(grad_tensor.std(0).square().sum())
-    print(f'{model.name:20}  {str(grad_tensor.shape[-1]):10}    {std:f}')
+    var = float(grad_tensor.std(dim=0).sum())
+    
+    print('[\'' +  model.name + '\',', str(var)+'],')
+    
