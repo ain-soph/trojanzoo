4b23a0be709e0bdf223ce78aec5d1c23a6342d45
Ren Pang <ain-soph@live.com>
2021-05-23 14:28:26 -0400

    add random model

--- trojanvision/utils/model_archs/darts/genotypes.py
+++ trojanvision/utils/model_archs/darts/genotypes.py
@@ -183,3 +183,11 @@ diy_deep_noskip = Genotype(
     reduce=[('max_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 2), ('max_pool_3x3', 1),
             ('max_pool_3x3', 0), ('skip_connect', 2), ('skip_connect', 2), ('max_pool_3x3', 1)],
     reduce_concat=[2, 3, 4, 5])
+
+random = Genotype(
+    normal=[('max_pool_3x3', 0), ('dil_conv_3x3', 1), ('sep_conv_3x3', 2), ('skip_connect', 1),
+            ('skip_connect', 3), ('avg_pool_3x3', 0), ('sep_conv_3x3', 1), ('dil_conv_3x3', 2)],
+    normal_concat=[2, 3, 4, 5],
+    reduce=[('avg_pool_3x3', 0), ('max_pool_3x3', 1), ('skip_connect', 1), ('dil_conv_3x3', 1),
+            ('sep_conv_3x3', 3), ('dil_conv_3x3', 1), ('max_pool_3x3', 0), ('sep_conv_3x3', 4)],
+    reduce_concat=[2, 3, 4, 5])
