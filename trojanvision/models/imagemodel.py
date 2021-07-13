075c0a8aebabe22971c950289a865d887839005a
Ren Pang <ain-soph@live.com>
2021-07-13 06:32:29 -0400

    update adversarial training

--- trojanvision/models/imagemodel.py
+++ trojanvision/models/imagemodel.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 from trojanvision.datasets import ImageSet
-from trojanzoo.models import _Model, Model
+from trojanvision.optim import PGD
 from trojanvision.utils import apply_cmap
+from trojanzoo.models import _Model, Model
 
 import torch
 import torch.nn as nn
@@ -96,10 +97,11 @@ class ImageModel(Model):
             self.suffix += '_adv_train'
             if 'suffix' not in self.param_list['model']:
                 self.param_list['model'].append('suffix')
+            self.pgd = PGD(pgd_alpha=self.adv_train_eval_alpha, pgd_eps=self.adv_train_eval_eps,
+                           iteration=self.adv_train_eval_iter, stop_threshold=None)
+            self._ce_loss_fn = nn.CrossEntropyLoss(weight=self.loss_weights)
         self._model: _ImageModel
         self.dataset: ImageSet
-        self.pgd = None  # TODO: python 3.10 type annotation
-        self._ce_loss_fn = nn.CrossEntropyLoss(weight=self.loss_weights)
 
     @classmethod
     def get_name(cls, name: str, layer: int = None) -> str:
@@ -112,19 +114,6 @@ class ImageModel(Model):
             partial_name += str(layer)
         full_list[0] = partial_name
         return '_'.join(full_list)
-
-    def adv_loss(self, _input: torch.Tensor, _label: torch.Tensor) -> torch.Tensor:
-        _output = self(_input)
-        return -self._ce_loss_fn(_output, _label)
-
-    def get_data(self, data: tuple[torch.Tensor, torch.Tensor], adv: bool = False, **kwargs) -> tuple[torch.Tensor, torch.Tensor]:
-        if adv:
-            assert self.pgd is not None
-            _input, _label = super().get_data(data, **kwargs)
-            adv_loss_fn = functools.partial(self.adv_loss, _label=_label)
-            adv_x, _ = self.pgd.optimize(_input=_input, loss_fn=adv_loss_fn)
-            return adv_x, _label
-        return super().get_data(data, **kwargs)
 
     # TODO: requires _input shape (N, C, H, W)
     # Reference: https://keras.io/examples/vision/grad_cam/
@@ -164,7 +153,31 @@ class ImageModel(Model):
         heatmap = apply_cmap(heatmap.detach().cpu(), cmap)
         return heatmap[0] if squeeze_flag else heatmap
 
+    def _adv_loss_helper(self, _input: torch.Tensor, _label: torch.Tensor) -> torch.Tensor:
+        _output = self(_input)
+        return -self._ce_loss_fn(_output, _label)
+
+    def get_data(self, data: tuple[torch.Tensor, torch.Tensor], adv_train: bool = False, **kwargs) -> tuple[torch.Tensor, torch.Tensor]:
+        if adv_train:
+            assert self.pgd is not None
+            _input, _label = super().get_data(data, **kwargs)
+            adv_loss_fn = functools.partial(self._adv_loss_helper, _label=_label)
+            adv_x, _ = self.pgd.optimize(_input=_input, loss_fn=adv_loss_fn)
+            return adv_x, _label
+        return super().get_data(data, **kwargs)
+
+    def _validate(self, adv_train: bool = None, **kwargs) -> tuple[float, float]:
+        adv_train = adv_train if adv_train is not None else self.adv_train
+        if not adv_train:
+            return super()._validate(**kwargs)
+        _, clean_acc = super()._validate(print_prefix='Validate Clean', main_tag='valid clean',
+                                         adv_train=False, **kwargs)
+        _, adv_acc = super()._validate(print_prefix='Validate Adv', main_tag='valid adv',
+                                       adv_train=True, **kwargs)
+        return adv_acc, clean_acc + adv_acc
+
     def _train(self, epoch: int, optimizer: Optimizer, lr_scheduler: _LRScheduler = None,
+               adv_train: bool = None,
                print_prefix: str = 'Epoch', start_epoch: int = 0, resume: int = 0,
                validate_interval: int = 10, save: bool = False, amp: bool = False,
                loader_train: torch.utils.data.DataLoader = None, loader_valid: torch.utils.data.DataLoader = None,
@@ -177,15 +190,12 @@ class ImageModel(Model):
                writer=None, main_tag: str = 'train', tag: str = '',
                accuracy_fn: Callable[..., list[float]] = None,
                verbose: bool = True, indent: int = 0, **kwargs):
-        if self.adv_train:
+        adv_train = adv_train if adv_train is not None else self.adv_train
+        if adv_train:
             after_loss_fn_old = after_loss_fn
             if not callable(after_loss_fn) and hasattr(self, 'after_loss_fn'):
                 after_loss_fn_old = getattr(self, 'after_loss_fn')
-            validate_fn_old = validate_fn if callable(validate_fn) else self._validate
             loss_fn = loss_fn if callable(loss_fn) else self.loss
-            from trojanvision.optim import PGD  # TODO: consider to move import sentences to top of file
-            self.pgd = PGD(pgd_alpha=self.adv_train_eval_alpha, pgd_eps=self.adv_train_eval_eps,
-                           iteration=self.adv_train_eval_iter, stop_threshold=None)
 
             def after_loss_fn_new(_input: torch.Tensor, _label: torch.Tensor, _output: torch.Tensor,
                                   loss: torch.Tensor, optimizer: Optimizer, loss_fn: Callable[..., torch.Tensor] = None,
@@ -193,9 +203,21 @@ class ImageModel(Model):
                 noise = torch.zeros_like(_input)
                 if self.adv_train_random_init:
                     noise.uniform_(-self.adv_train_eps, self.adv_train_eps)
-                adv_loss_fn = functools.partial(self.adv_loss, _label=_label)
+                adv_loss_fn = functools.partial(self._adv_loss_helper, _label=_label)
 
-                for m in range(self.pgd.iteration):
+                if after_loss_fn_old is None and not self.adv_train_free:
+                    self.eval()
+                    adv_x, _ = self.pgd.optimize(_input=_input, noise=noise, loss_fn=adv_loss_fn,
+                                                 iteration=self.adv_train_iter, pgd_alpha=self.adv_train_alpha, pgd_eps=self.adv_train_eps)
+                    self.train()
+                    loss = loss_fn(adv_x, _label)
+                    if amp:
+                        scaler.scale(loss).backward()
+                    else:
+                        loss.backward()
+                    return
+
+                for m in range(self.adv_train_iter):
                     if self.adv_train_free:
                         if amp:
                             scaler.step(optimizer)
@@ -216,18 +238,10 @@ class ImageModel(Model):
                     else:
                         loss.backward()
 
-            def validate_fn_new(get_data_fn: Callable[..., tuple[torch.Tensor, torch.Tensor]] = None,
-                                print_prefix: str = 'Validate', **kwargs) -> tuple[float, float]:
-                _, clean_acc = validate_fn_old(print_prefix='Validate Clean', main_tag='valid clean',
-                                               get_data_fn=None, **kwargs)
-                _, adv_acc = validate_fn_old(print_prefix='Validate Adv', main_tag='valid adv',
-                                             get_data_fn=functools.partial(get_data_fn, adv=True), **kwargs)
-                return adv_acc, clean_acc + adv_acc
-
             after_loss_fn = after_loss_fn_new
-            validate_fn = validate_fn_new
 
         super()._train(epoch=epoch, optimizer=optimizer, lr_scheduler=lr_scheduler,
+                       adv_train=adv_train,
                        print_prefix=print_prefix, start_epoch=start_epoch, resume=resume,
                        validate_interval=validate_interval, save=save, amp=amp,
                        loader_train=loader_train, loader_valid=loader_valid,
