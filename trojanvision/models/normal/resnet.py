fb70623514e127f0b28bc721c72ac41830c54784
Ren Pang <ain-soph@live.com>
2021-07-19 03:43:11 -0400

    implement resnet_ap

--- trojanvision/models/normal/resnet.py
+++ trojanvision/models/normal/resnet.py
@@ -1,19 +1,34 @@
 #!/usr/bin/env python3
 
 from trojanvision.models.imagemodel import _ImageModel, ImageModel
+from trojanvision.datasets import ImageSet
+import trojanvision.utils.model_archs.resnet_ap as resnet_ap
 
 import torch
 import torch.nn as nn
 import torchvision.models
 from torchvision.models.resnet import model_urls as urls
+from torchvision.models.resnet import conv3x3
+import argparse
 from collections import OrderedDict
-from collections.abc import Callable
+from typing import Callable, Union
+
+
+def GroupNorm(num_groups: int, num_channels: int = None, eps: float = 1e-5, affine: bool = True,
+              device=None, dtype=None) -> nn.GroupNorm:
+    num_channels = num_groups if num_channels is None else num_channels
+    return nn.GroupNorm(num_groups, num_channels, eps=eps, affine=affine, device=device, dtype=dtype)
 
 
 class _ResNet(_ImageModel):
 
-    def __init__(self, name: str = 'resnet18', **kwargs):
+    def __init__(self, name: str = 'resnet18', dataset: ImageSet = None, data_shape: list[int] = None,
+                 norm_layer: Callable[..., nn.Module] = None, **kwargs):
         super().__init__(**kwargs)
+        if data_shape is None:
+            assert isinstance(dataset, ImageSet)
+            data_shape = dataset.data_shape
+        channel = data_shape[0]
         module_list: list[nn.Module] = []
         if 's' in name.split('_'):
             from trojanvision.utils.model_archs.resnet_s import resnet_s
@@ -25,13 +40,15 @@ class _ResNet(_ImageModel):
                 ('fc', _model.fc)
             ]))
         else:
-            model_class = name.replace('_comp', '').replace('_s', '')
-            ModelClass: Callable[..., torchvision.models.ResNet] = getattr(torchvision.models, model_class)
-            _model = ModelClass(num_classes=self.num_classes)
+            model_class = name.replace('_ap', '').replace('_comp', '').replace('_s', '')
+            module = resnet_ap if 'ap' in name else torchvision.models
+            ModelClass = getattr(module, model_class)
+            kwargs = {'pool_size': data_shape[1] // 8} if 'ap' in name else {}
+            _model: Union[resnet_ap.ResNet_AP, torchvision.models.ResNet] = ModelClass(num_classes=self.num_classes,
+                                                                                       norm_layer=norm_layer, **kwargs)
             if 'comp' in name:
                 conv1: nn.Conv2d = _model.conv1
-                _model.conv1 = nn.Conv2d(conv1.in_channels, conv1.out_channels,
-                                         kernel_size=3, stride=1, padding=1, bias=False)
+                _model.conv1 = conv3x3(channel, conv1.out_channels)
                 if 'resnext' in name:
                     _model.fc = nn.Linear(_model.fc.in_features // 2,
                                           _model.fc.out_features,
@@ -56,6 +73,12 @@ class _ResNet(_ImageModel):
         if not ('comp' in name and 'resnext' in name):
             module_list.append(('layer4', _model.layer4))
         self.features = nn.Sequential(OrderedDict(module_list))
+        if channel != 3:
+            conv: nn.Conv2d = self.features.conv1
+            if conv.in_channels != channel:
+                keys = ['out_channels', 'kernel_size', 'stride', 'padding']
+                kwargs = {key: getattr(conv, key) for key in keys}
+                self.features.conv1 = nn.Conv2d(in_channels=channel, bias=False, **kwargs)
 
 
 class ResNet(ImageModel):
@@ -64,13 +87,23 @@ class ResNet(ImageModel):
                         'resnet18_comp', 'resnet34_comp', 'resnet50_comp', 'resnet101_comp', 'resnet152_comp',
                         'resnet18_s', 'resnet34_s', 'resnet50_s', 'resnet101_s', 'resnet152_s',
                         'resnext50_32x4d', 'resnext101_32x8d', 'wide_resnet50_2', 'wide_resnet101_2',
-                        'resnext50_32x4d_comp', 'resnext101_32x8d_comp', 'wide_resnet50_2_comp', 'wide_resnet101_2_comp']
+                        'resnext50_32x4d_comp', 'resnext101_32x8d_comp', 'wide_resnet50_2_comp', 'wide_resnet101_2_comp',
+                        'resnet18_ap_comp']
 
     model_urls = urls
 
+    @classmethod
+    def add_argument(cls, group: argparse._ArgumentGroup):
+        super().add_argument(group)
+        group.add_argument('--norm_layer', choices=['bn', 'gn'], default='bn')
+        return group
+
     def __init__(self, name: str = 'resnet', layer: int = 18,
-                 model: type[_ResNet] = _ResNet, **kwargs):
-        super().__init__(name=name, layer=layer, model=model, **kwargs)
+                 model: type[_ResNet] = _ResNet,
+                 norm_layer: Union[str, Callable[..., nn.Module]] = 'bn', **kwargs):
+        if isinstance(norm_layer, str):
+            norm_layer = nn.BatchNorm2d if norm_layer == 'bn' else GroupNorm
+        super().__init__(name=name, layer=layer, model=model, norm_layer=norm_layer, **kwargs)
 
     def get_official_weights(self, **kwargs) -> OrderedDict[str, torch.Tensor]:
         _dict = super().get_official_weights(**kwargs)
---
+++ trojanvision/utils/model_archs/resnet_ap.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+# https://github.com/facebookresearch/GradientEpisodicMemory/blob/master/model/common.py
+
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+import torch
+import torch.nn as nn
+from torchvision.models.resnet import conv1x1, conv3x3, ResNet
+
+from typing import Any, Callable, Optional, Union
+
+
+class BasicBlock_AP(nn.Module):
+    expansion: int = 1
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv3x3(inplanes, planes, stride=1)    # modification
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.pool = nn.Identity() if stride == 1 else nn.AvgPool2d(kernel_size=stride, stride=stride)   # modification
+
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = norm_layer(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.pool(out)    # modification
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck_AP(nn.Module):
+    # Bottleneck in torchvision places the stride for downsampling at 3x3 convolution(self.conv2)
+    # while original implementation places the stride at the first 1x1 convolution(self.conv1)
+    # according to "Deep residual learning for image recognition"https://arxiv.org/abs/1512.03385.
+    # This variant is also known as ResNet V1.5 and improves accuracy according to
+    # https://ngc.nvidia.com/catalog/model-scripts/nvidia:resnet_50_v1_5_for_pytorch.
+
+    expansion: int = 4
+
+    def __init__(
+        self,
+        inplanes: int,
+        planes: int,
+        stride: int = 1,
+        downsample: Optional[nn.Module] = None,
+        groups: int = 1,
+        base_width: int = 64,
+        dilation: int = 1,
+        norm_layer: Optional[Callable[..., nn.Module]] = None
+    ) -> None:
+        super().__init__()
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        width = int(planes * (base_width / 64.)) * groups
+        # Both self.conv2 and self.downsample layers downsample the input when stride != 1
+        self.conv1 = conv1x1(inplanes, width)
+        self.bn1 = norm_layer(width)
+        self.conv2 = conv3x3(width, width, stride=1, groups=groups, dilation=dilation)  # modification
+        self.bn2 = norm_layer(width)
+        self.pool = nn.Identity() if stride == 1 else nn.AvgPool2d(kernel_size=stride, stride=stride)   # modification
+        self.conv3 = conv1x1(width, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.pool(out)    # modification
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet_AP(ResNet):
+    def __init__(self, *args, pool_size: int = 4, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.avgpool = nn.Identity()    # modification
+        self.fc = nn.Linear(self.fc.in_features * (pool_size * pool_size), self.fc.out_features)    # modification
+
+    def _make_layer(self, block: type[Union[BasicBlock_AP, Bottleneck_AP]], planes: int, blocks: int,
+                    stride: int = 1, dilate: bool = False) -> nn.Sequential:
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride=1),  # modification
+                nn.AvgPool2d(kernel_size=stride, stride=stride),    # modification
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return nn.Sequential(*layers)
+
+
+def _resnet(
+    block: type[Union[BasicBlock_AP, Bottleneck_AP]],
+    layers: list[int],
+    **kwargs: Any
+) -> ResNet_AP:
+    return ResNet_AP(block, layers, **kwargs)
+
+
+def resnet18(**kwargs: Any) -> ResNet_AP:
+    r"""ResNet-18 model from
+    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_.
+
+    """
+    return _resnet(BasicBlock_AP, [2, 2, 2, 2], **kwargs)
