1b390099c1464b649bfe8c1744152202b4039c38
Ren Pang <ain-soph@live.com>
2021-07-19 03:43:57 -0400

    update weight_init

--- trojanvision/utils/model.py
+++ trojanvision/utils/model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import torch
-import torch.nn.init as init
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.modules.conv import _ConvNd
@@ -20,30 +19,8 @@ def weight_init(m: nn.Module, filter_list: tuple[type] = ()) -> None:
     '''
     if isinstance(m, filter_list):
         return
-
     if hasattr(m, 'reset_parameters'):
         return m.reset_parameters()
-    if isinstance(m, (nn.Conv1d, nn.ConvTranspose1d)):
-        init.normal_(m.weight.data)
-        if m.bias is not None:
-            init.normal_(m.bias.data)
-    elif isinstance(m, (nn.Conv2d, nn.Conv3d,
-                        nn.ConvTranspose2d, nn.ConvTranspose3d)):
-        init.xavier_normal_(m.weight.data)
-        if m.bias is not None:
-            init.normal_(m.bias.data)
-    elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d, nn.BatchNorm3d)):
-        init.normal_(m.weight.data, mean=1, std=0.02)
-        init.constant_(m.bias.data, 0)
-    elif isinstance(m, nn.Linear):
-        init.xavier_normal_(m.weight.data)
-        init.normal_(m.bias.data)
-    elif isinstance(m, (nn.LSTM, nn.LSTMCell, nn.GRU, nn.GRUCell)):
-        for param in m.parameters():
-            if param.dim() >= 2:
-                init.orthogonal_(param.data)
-            else:
-                init.normal_(param.data)
     else:
         for layer in m.children():
             weight_init(layer, filter_list=filter_list)
