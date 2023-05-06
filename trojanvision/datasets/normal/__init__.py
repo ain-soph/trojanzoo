c2509b676e7d5450efcbfd669bff3910f460445e
ain-soph <ain-soph@live.com>
2023-05-05 20:48:23 -0400

    add celeba dataset

--- trojanvision/datasets/normal/__init__.py
+++ trojanvision/datasets/normal/__init__.py
@@ -3,6 +3,7 @@
 from trojanvision.datasets.imageset import ImageSet
 
 from .cifar import CIFAR10, CIFAR100
+from .celeba import CelebA
 from .downsampled_imagenet import ImageNet16, ImageNet32, ImageNet64
 from .mnist import MNIST
 from .stl10 import STL10
@@ -12,6 +13,7 @@ __all__ = ['MNIST', 'CIFAR10', 'CIFAR100', 'CelebA', 'ImageNet16', 'ImageNet32',
 class_dict: dict[str, ImageSet] = {
     'cifar10': CIFAR10,
     'cifar100': CIFAR100,
+    'celeba': CelebA,
     'imagenet16': ImageNet16,
     'imagenet32': ImageNet32,
     'imagenet64': ImageNet64,
---
+++ trojanvision/datasets/normal/celeba.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from trojanvision.datasets.imageset import ImageSet
+
+import torchvision.datasets as datasets
+
+
+class CelebA(ImageSet):
+    r"""CelebA dataset.
+    It inherits :class:`trojanvision.datasets.ImageSet`.
+
+    See Also:
+        * torchvision: :any:`torchvision.datasets.CelebA`
+        * paper: `Deep Learning Face Attributes in the Wild`_
+        * website: http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html
+
+    Attributes:
+        name (str): ``'celeba'``
+        num_classes (int): ``10``
+        data_shape (list[int]): ``[1, 28, 28]``
+        norm_par (dict[str, list[float]]):
+            ``{'mean': [0.1307], 'std': [0.3081]}``
+
+    .. _Deep Learning Face Attributes in the Wild:
+        https://arxiv.org/abs/1411.7766
+    """
+
+    name: str = 'celeba'
+    num_classes: int = 10177
+    data_shape = [3, 64, 64]
+
+    def __init__(self, norm_par: dict[str, list[int]] = {'mean': [0.5, 0.5, 0.5], 'std': [0.5, 0.5, 0.5]}, **kwargs):
+        super().__init__(norm_par=norm_par, **kwargs)
+
+    def initialize(self):
+        datasets.CelebA(root=self.folder_path, split='all', download=True)
+
+    def _get_org_dataset(self, mode, **kwargs):
+        return datasets.CelebA(root=self.folder_path, split=mode, target_type='identity', **kwargs)
