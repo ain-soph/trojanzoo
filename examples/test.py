46d77868f7a4678fafcedbd7425cf8ddb3ea5e29
ain-soph <ain-soph@live.com>
2024-01-14 22:32:23 -0500

    update test

--- examples/test.py
+++ examples/test.py
@@ -1,10 +1,26 @@
-# Used for python interactive window
+#!/usr/bin/env python3
 
+r"""
+CUDA_VISIBLE_DEVICES=0 python examples/test.py --color --verbose 1 --dataset cifar10 --pretrained --model resnet18_comp
+"""  # noqa: E501
+
+import torch
 import trojanvision
+import argparse
 
-env = trojanvision.environ.create(verbose=1, color=True, tqdm=True)
-dataset = trojanvision.datasets.create()
-model = trojanvision.models.create(dataset=dataset)
-trainer = trojanvision.trainer.create(dataset=dataset, model=model)
-mark = trojanvision.marks.create(dataset=dataset)
-attack = trojanvision.attacks.create('trojannn', dataset=dataset, model=model, mark=mark)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    trojanvision.environ.add_argument(parser)
+    trojanvision.datasets.add_argument(parser)
+    trojanvision.models.add_argument(parser)
+    kwargs = vars(parser.parse_args())
+
+    env = trojanvision.environ.create(**kwargs)
+    dataset = trojanvision.datasets.create(**kwargs)
+    model = trojanvision.models.create(dataset=dataset, **kwargs)
+
+    if env['verbose']:
+        trojanvision.summary(env=env, dataset=dataset, model=model)
+
+    a = torch.rand(1, 3, 32, 32).to(device=env['device'])
+    model.get_all_layer(a, verbose=2)
